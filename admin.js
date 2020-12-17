d18350c1b0bbf4a3cc8939d84917cabbf36375a9
Gabriel Neri <gasantosneri@hotmail.com>
2020-12-17 20:21:17 -0300

    add register field

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
-const newProduct = document.querySelector('.cadastro');
+const newProduct = document.querySelector('.cadastro-prod');
 const modalProduct = document.querySelector('.new-prod');
 const modalClose = document.querySelector('.new-prod-close');
 
 newProduct.addEventListener('click', function() {
     console.log("aqui")
-    modalProduct.classList.add('bg-active-2');
+    modalProduct.classList.add('bg-active-prod');
 });
 
 modalClose.addEventListener('click', function () {
-    modalProduct.classList.remove('bg-active-2');
+    modalProduct.classList.remove('bg-active-prod');
 });
--- app.js
+++ app.js
@@ -9,3 +9,15 @@ login.addEventListener('click', function () {
 modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
 });
+
+const cadastrar = document.querySelector('.cadastrar');
+const modalCadastro = document.querySelector('.cadastro');
+const modalCadastroClose = document.querySelector('.cadastro-close');
+
+cadastrar.addEventListener('click', function () {
+    modalCadastro.classList.add('bg-active-cadastro');
+});
+
+modalCadastroClose.addEventListener('click', function () {
+    modalCadastro.classList.remove('bg-active-cadastro');
+});
