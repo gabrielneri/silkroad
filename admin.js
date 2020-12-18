ee95ebf033def334ca7beb0589297cc8eaf37c6c
Gabriel Neri <gasantosneri@hotmail.com>
2020-12-17 21:19:20 -0300

    add new category

--- admin.js
+++ admin.js
@@ -3,10 +3,22 @@ const modalProduct = document.querySelector('.new-prod');
 const modalClose = document.querySelector('.new-prod-close');
 
 newProduct.addEventListener('click', function() {
-    console.log("aqui")
     modalProduct.classList.add('bg-active-prod');
 });
 
 modalClose.addEventListener('click', function () {
     modalProduct.classList.remove('bg-active-prod');
+});
+
+const newCategory = document.querySelector('.register-category');
+const modalCategory = document.querySelector('.new-category');
+const modalCloseCategory = document.querySelector('.new-category-close');
+
+newCategory.addEventListener('click', function() {
+    console.log("alo" + modalCategory)
+    modalCategory.classList.add('bg-active-category');
+});
+
+modalCloseCategory.addEventListener('click', function () {
+    modalCategory.classList.remove('bg-active-category');
 });
