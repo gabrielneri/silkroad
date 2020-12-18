05484781107731ad0869d2082688cf696e456294
Gabriel Neri <gasantosneri@hotmail.com>
2020-12-18 00:48:30 -0300

    add login page

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-const login = document.querySelector('.login');
-const modalBg = document.querySelector('.modal-bg');
-const modalClose = document.querySelector('.modal-close');
-
-login.addEventListener('click', function () {
-    modalBg.classList.add('bg-active');
-});
-
-modalClose.addEventListener('click', function () {
-    modalBg.classList.remove('bg-active');
-});
-
 const cadastrar = document.querySelector('.cadastrar');
 const modalCadastro = document.querySelector('.cadastro');
 const modalCadastroClose = document.querySelector('.cadastro-close');
